b8095d4ddf46c43b0c0088b4249229e9b7c12246
Shan <shanpiv@gmail.com>
2023-10-16 16:21:54 +0300

    Fist Step (Connected to Reddis)

--- main.go
+++ main.go
@@ -1,1 +1,36 @@
-package LikeReddisByGo
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+func main() {
+	l, err := net.Listen("tcp", ":6379")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	println("Ожидание подключения")
+	conn, err := l.Accept()
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	println("Connected")
+	defer conn.Close()
+
+	for {
+		data := make([]byte, 1024)
+		_, err = conn.Read(data)
+		if err != nil {
+			if err == io.EOF {
+				fmt.Println("Bye-bye user")
+				break
+			}
+			fmt.Println("Cant user input")
+		}
+		conn.Write([]byte("+ShutUp\r\n"))
+	}
+}
